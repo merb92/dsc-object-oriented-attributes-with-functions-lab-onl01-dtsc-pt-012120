f58e5755ad96b732973fd2cb9749b2ea3936db33
Mike Erb <merb92@gmail.com>
2020-05-19 19:01:32 -0400

    finished lab

--- school.py
+++ school.py
@@ -1,1 +1,21 @@
 class School:
+    def __init__(self, name, roster={}):
+        self.name = name
+        self.roster = roster
+
+    def add_student(self, full_name, grade_level):
+        if self.roster.get(grade_level) != None:
+            # the grade already exists
+            self.roster[grade_level].append(full_name)
+        else:
+            # the grade does not exist yet
+            self.roster[grade_level] = [full_name]
+
+    def grade(self, grade):
+        grade_info = self.roster.get(grade)
+        return grade_info
+
+    def sort_roster(self):
+        for key, value in self.roster.items():
+            self.roster[key] = sorted(value)
+        return self.roster
